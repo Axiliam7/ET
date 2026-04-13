a60df454d5ded894affd8d26c40433022840217d
Axiliam7 <166849070+Axiliam7@users.noreply.github.com>
2026-04-13 23:51:24 +0530

    Create server.js for the backend

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
